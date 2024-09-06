1bc951e88a1dd69613b27aaa08a949d7265d5911
bhargavisreevathsa <42955441+bhargavisreevathsa@users.noreply.github.com>
2024-09-06 23:10:16 +0530

    Update thread_monitor.sh

--- thread_monitor.sh
+++ thread_monitor.sh
@@ -4,7 +4,7 @@ pid=$1;
 while(true)
 do
 
-`/usr/local/bin/jattach $pid threaddump > /var/log/netwitness/esadumps/esa_dump_$i.txt`;
+`/usr/local/bin/jattach $pid threaddump > /var/log/thread_dumps/$i.txt`;
 
 sleep 5m
 
